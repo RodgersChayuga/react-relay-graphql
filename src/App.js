6e67c09345b4eab22ddb84f2966e17d979e5a77c
Rodgers Chayuga <chayugarodgers@gmail.com>
2024-02-23 00:34:56 +0300

    Fetching GraphQL From React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
 import React from "react";
 
-const App = () => {
-  return <div>App</div>;
-};
+import fetchGraphQL from "./helpers/fetchGraphQL";
+
+const { useState, useEffect } = React;
+
+function App() {
+  // We'll load the name of a repository, initially setting it to null
+  const [name, setName] = useState(null);
+
+  // When the component mounts we'll fetch a repository name
+  useEffect(() => {
+    let isMounted = true;
+    fetchGraphQL(`
+      query RepositoryNameQuery {
+        # feel free to change owner/name here
+        repository(owner: "facebook" name: "relay") {
+          name
+        }
+      }
+    `)
+      .then((response) => {
+        // Avoid updating state if the component unmounted before the fetch completes
+        if (!isMounted) {
+          return;
+        }
+        const data = response.data;
+        setName(data.repository.name);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  // Render "Loading" until the query completes
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{name != null ? `Repository: ${name}` : "Loading"}</p>
+      </header>
+    </div>
+  );
+}
 
 export default App;
